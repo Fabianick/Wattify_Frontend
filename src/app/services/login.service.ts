ceb4b48c75b1fdb3a9db4ee10d0b7e285ff090a4
Fabianick <fabianick2014@gmail.com>
2023-11-21 18:29:11 -0500

    Revert "Revert "deploy1""
    This reverts commit 1bbc9407371355c1fce8c3ac7317cddcbc1b6288.

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,7 +9,7 @@ export class LoginService {
 
   constructor(private http: HttpClient) { }
   login(request: JwtRequest) {
-    return this.http.post("http://localhost:8080/authenticate", request);
+    return this.http.post("https://frontedwattify.web.app/authenticate", request);
   }
   verificar() {
     let token = sessionStorage.getItem("token");
